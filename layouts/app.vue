3c048dd66e58d8f2d6ba5b10f09e1a09e0ecc22b
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2022-03-02 19:08:25 -0800

    sidebar

---
+++ layouts/app.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="h-screen flex flex-row font-cursive w-screen overflow-auto bg-gray-900">
+    <div class="h-screen sticky top-0 w-56 bg-amber-500 pt-10">
+     <h1 class="text-3xl font-bold text-gray-900 block text-center italic">Pickupline</h1>
+     <hr class="border-2 border-amber-900/20"/>
+    </div>
+    <Nuxt class="text-white"/>
+</div>
+</template>
---
+++ pages/app.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <div :key="n" v-for="n in 100">
+        {{n}}
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    layout: 'app'
+}
+</script>
