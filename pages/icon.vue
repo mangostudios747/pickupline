5fd86001b485f1a1487965acb2c1fafbf9cb0e9b
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2022-03-03 21:07:35 -0800

    icon

---
+++ pages/icon.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-10 h-full w-full">
+    <div class="text-amber-500 w-[256px] h-[256px] border-red border text-[200px] italic font-bold text-center flex box-content">
+      <span class="my-auto align-middle leading-[0.5] mx-auto">P</span>
+    </div>
+  </div>
+</template>
